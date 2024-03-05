a260841d63a2ff80423310a6aa488ef52585500c
KevinAraujo12 <kevinnunes20011210@gmail.com>
2024-03-05 17:59:04 -0300

    📝:Kevin pular o cone

--- Estudando js com jogos/Kevin Skate/js/script.js
+++ Estudando js com jogos/Kevin Skate/js/script.js
@@ -1,5 +1,5 @@
 const kevin = document.querySelector(".kevin-skate");
-console.log(kevin);
+const cone = document.querySelector(".cone");
 
 const jump = () => {
     kevin.classList.add("jump");
@@ -8,6 +8,20 @@ const jump = () => {
         kevin.classList.remove("jump");
     }, 500);
 } 
+
+loop = setInterval(() => {
+
+    const conePosition = cone.offsetLeft; 
+    const kevinPosition = +window.getComputedStyle(kevin).bottom.replace("px", "");
+    
+    if (conePosition <= 120 && conePosition > 0 && kevinPosition < 120) {
+
+        cone.style.animation = "none";
+        cone.style.left = `${conePosition}px`;
+
+    }
+
+},10);
 
 document.addEventListener("keydown", jump);
 
