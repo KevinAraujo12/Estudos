5efbb73ff6a3c904c9fd130bd8e002a21c8797b8
KevinAraujo12 <kevinnunes20011210@gmail.com>
2024-03-08 18:06:49 -0300

    🚀:Ciclo de vida dos componentes

--- React/my-app/src/App.js
+++ React/my-app/src/App.js
@@ -1,16 +1,30 @@
 import React, { Component } from "react";
 
+// Ciclo de vida dos componentes.
 
 class App extends Component {
 
     constructor(props){
         super(props);
+        this.state = {
+            hora: "00:00:00"
+        }
+    }
+
+    componentDidMount(){
+        setInterval(()=> {
+            this.setState({ hora: new Date().toLocaleTimeString() })
+        }, 1000);
+    }
+
+    componentDidUpdate() {
+        console.log("Atualizou!!!");
     }
 
     render(){
         return(
             <div>
-
+                <h1>Meu projeto -- {this.state.hora}</h1>
             </div>
         );
     }
