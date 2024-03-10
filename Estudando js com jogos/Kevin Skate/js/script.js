5d121564dc361897372bf9c236075b6721480d04
KevinAraujo12 <kevinnunes20011210@gmail.com>
2024-03-10 00:45:01 -0300

    🚀:Contador de pulos, Falta resolve bug

--- Estudando js com jogos/Kevin Skate/js/script.js
+++ Estudando js com jogos/Kevin Skate/js/script.js
@@ -1,12 +1,17 @@
 const kevin = document.querySelector(".kevin-skate");
 const cone = document.querySelector(".cone");
 
+let contador = 0;
 const jump = () => {
     kevin.classList.add("jump");
-
+    const contadorElemento = document.getElementById("Contador"); 
+    
+    contador = contador + 1;
     setTimeout(() => {
         kevin.classList.remove("jump");
     }, 500);
+     console.log(contador);
+     contadorElemento.textContent = contador;
 } 
 
 loop = setInterval(() => {
@@ -15,13 +20,19 @@ loop = setInterval(() => {
     const kevinPosition = +window.getComputedStyle(kevin).bottom.replace("px", "");
     
     if (conePosition <= 120 && conePosition > 0 && kevinPosition < 120) {
-
+        
         cone.style.animation = "none";
         cone.style.left = `${conePosition}px`;
-
+        
     }
-
+    
 },10);
 
+// contador de cones pulados
+
+
+
+
 document.addEventListener("keydown", jump);
+
 
